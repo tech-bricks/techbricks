a8adca9bd2e78938652f875fdbcbf2ecb97e9438
Sandeep Modi <sanmodi@deloitte.com>
2025-12-16 16:41:53 +0530

    updated

--- TechBricks/Helper/EmailSender.cs
+++ TechBricks/Helper/EmailSender.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.Extensions.Options;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
+using Microsoft.Extensions.Options;
 using TechBricks.Models;
 
 namespace TechBricks.Helper
